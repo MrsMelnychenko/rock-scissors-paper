ed248aa50f490805246856c54e4f3766a2164ba6
Mariana <melnychenko.mariana@gmail.com>
2022-12-25 20:07:54 +0200

    started adding setTimeout to jumping avatars

--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
 const clickPaper = document.querySelector(".paper");
 const playerAvatar = document.querySelector(".player-avatar");
-
-function changeCounter() {
-  alert("You win!");
-}
+const mottoFooter = document.querySelector(".choice");
 
 function addToCounter() {
   document.querySelector(".player-js-counter").textContent += 1;
 }
-
 // function jumpingPlayer() {
-//   document.querySelector(".player-avatar").top = 0;
+//   playerAvatar.style.marginTop = "-20px";
 // }
-clickPaper.addEventListener("click", changeCounter);
+// let i = 1;
+// setTimeout(function jumpingPlayer() {
+//   func(i);
+//   setTimeout(jumpingPlayer, 1000);
+// }, 1000);
+// setTimeout(function jumpingPlayerBack() {
+//   playerAvatar.style.marginTop = "0px";
+// }, 1000);
+function mottoWin() {
+  mottoFooter.textContent = "You Win!";
+  mottoFooter.style.color = "green";
+}
+setTimeout(function resetMotto() {
+  mottoFooter.textContent = "Make your choice!";
+  mottoFooter.style.color = "black";
+}, 5000);
+
 clickPaper.addEventListener("click", addToCounter);
+clickPaper.addEventListener("click", mottoWin);
+clickPaper.addEventListener("click", resetMotto);
+// clickPaper.addEventListener("click", jumpingPlayer);
+// clickPaper.addEventListener("click", jumpingPlayerBack);
