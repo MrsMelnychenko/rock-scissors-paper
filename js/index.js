81eb7eec1840de715274eefa7d65b2963dda28c2
Mariana <melnychenko.mariana@gmail.com>
2022-12-26 13:18:18 +0200

    made player avatar jump

--- js/index.js
+++ js/index.js
@@ -5,28 +5,27 @@ const mottoFooter = document.querySelector(".choice");
 function addToCounter() {
   document.querySelector(".player-js-counter").textContent += 1;
 }
-// function jumpingPlayer() {
-//   playerAvatar.style.marginTop = "-20px";
-// }
-// let i = 1;
-// setTimeout(function jumpingPlayer() {
-//   func(i);
-//   setTimeout(jumpingPlayer, 1000);
-// }, 1000);
-// setTimeout(function jumpingPlayerBack() {
-//   playerAvatar.style.marginTop = "0px";
-// }, 1000);
+function jumpingPlayer() {
+  playerAvatar.style.marginTop = "-20px";
+}
+function resetJump() {
+  playerAvatar.style.marginTop = "0px";
+}
 function mottoWin() {
   mottoFooter.textContent = "You Win!";
   mottoFooter.style.color = "green";
 }
-setTimeout(function resetMotto() {
+function resetMotto() {
   mottoFooter.textContent = "Make your choice!";
   mottoFooter.style.color = "black";
-}, 5000);
+}
 
 clickPaper.addEventListener("click", addToCounter);
 clickPaper.addEventListener("click", mottoWin);
-clickPaper.addEventListener("click", resetMotto);
-// clickPaper.addEventListener("click", jumpingPlayer);
-// clickPaper.addEventListener("click", jumpingPlayerBack);
+clickPaper.addEventListener("click", () => {
+  setTimeout(resetMotto, 2000);
+});
+clickPaper.addEventListener("click", jumpingPlayer);
+clickPaper.addEventListener("click", () => {
+  setTimeout(resetJump, 200);
+});
