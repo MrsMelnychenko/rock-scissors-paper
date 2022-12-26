fc63197e3d7536995291a67dd715c0086f2dd2e8
Mariana <melnychenko.mariana@gmail.com>
2022-12-26 14:35:15 +0200

    added template of game logic

--- js/index.js
+++ js/index.js
@@ -1,31 +1,112 @@
-const clickPaper = document.querySelector(".paper");
-const playerAvatar = document.querySelector(".player-avatar");
-const mottoFooter = document.querySelector(".choice");
+function game() {
+  const paper = document.querySelector(".paper");
+  const rock = document.querySelector(".rock");
+  const scissors = document.querySelector(".scissors");
+  const playerAvatar = document.querySelector(".player-avatar");
+  const mottoFooter = document.querySelector(".choice");
+  let playerWin = 0;
+  let computerWin = 0;
+  let playerLoss = 0;
+  let computerLoss = 0;
 
-function addToCounter() {
-  document.querySelector(".player-js-counter").textContent += 1;
-}
-function jumpingPlayer() {
-  playerAvatar.style.marginTop = "-20px";
-}
-function resetJump() {
-  playerAvatar.style.marginTop = "0px";
-}
-function mottoWin() {
-  mottoFooter.textContent = "You Win!";
-  mottoFooter.style.color = "green";
-}
-function resetMotto() {
-  mottoFooter.textContent = "Make your choice!";
-  mottoFooter.style.color = "black";
-}
+  function play() {
+    const playerOptions = [rock, paper, scissors];
+    const computerOptions = ["rock", "paper", "scissors"];
+    playerOptions.forEach((option) => {
+      option.addEventListener("click", function () {
+        const choiceNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[choiceNumber];
+        winner(this.innerText, computerChoice);
+      });
+    });
+  }
+  function winner(player, computer) {
+    player = player.toLowerCase();
+    computer = computer.toLowerCase();
+    if (player === computer) {
+      mottoDraw();
+    } else if (player == "rock") {
+      if (computer == "paper") {
+        mottoLose();
+        computerWin++;
+        playerLoss++;
+        document.querySelector(".player-loss-counter").textContent = playerLoss;
+        document.querySelector(".computer-win-counter").textContent =
+          computerWin;
+      } else {
+        mottoWin();
+        computerLoss++;
+        playerWin++;
+        document.querySelector(".player-loss-counter").textContent = playerWin;
+        document.querySelector(".computer-win-counter").textContent =
+          computerLoss;
+      }
+    } else if (player == "scissors") {
+      if (computer == "rock") {
+        mottoLose();
+        computerWin++;
+        playerLoss++;
+        document.querySelector(".player-loss-counter").textContent = playerLoss;
+        document.querySelector(".computer-win-counter").textContent =
+          computerWin;
+      } else {
+        mottoWin();
+        computerLoss++;
+        playerWin++;
+        document.querySelector(".player-loss-counter").textContent = playerWin;
+        document.querySelector(".computer-win-counter").textContent =
+          computerLoss;
+      }
+    } else if (player == "paper") {
+      if (computer == "scissors") {
+        mottoLose();
+        computerWin++;
+        playerLoss++;
+        document.querySelector(".player-loss-counter").textContent = playerLoss;
+        document.querySelector(".computer-win-counter").textContent =
+          computerWin;
+      } else {
+        mottoWin();
+        computerLoss++;
+        playerWin++;
+        document.querySelector(".player-loss-counter").textContent = playerWin;
+        document.querySelector(".computer-win-counter").textContent =
+          computerLoss;
+      }
+    }
+  }
 
-clickPaper.addEventListener("click", addToCounter);
-clickPaper.addEventListener("click", mottoWin);
-clickPaper.addEventListener("click", () => {
-  setTimeout(resetMotto, 2000);
-});
-clickPaper.addEventListener("click", jumpingPlayer);
-clickPaper.addEventListener("click", () => {
-  setTimeout(resetJump, 200);
-});
+  function mottoWin() {
+    mottoFooter.textContent = "You Win!";
+    mottoFooter.style.color = "green";
+  }
+  function mottoLose() {
+    mottoFooter.textContent = "You Lose!";
+    mottoFooter.style.color = "red";
+  }
+  function mottoDraw() {
+    mottoFooter.textContent = "Draw!";
+    mottoFooter.style.color = "grey";
+  }
+  function resetMotto() {
+    mottoFooter.textContent = "Make your choice!";
+    mottoFooter.style.color = "black";
+  }
+  //Animation of jumps
+  //   function jumpingPlayer() {
+  //     playerAvatar.style.marginTop = "-40px";
+  //   }
+  //   function resetJump() {
+  //     playerAvatar.style.marginTop = "0px";
+  //   }
+  //   paper.addEventListener("click", mottoWin);
+  //   paper.addEventListener("click", () => {
+  //     setTimeout(resetMotto, 2000);
+  //   });
+  //   paper.addEventListener("click", jumpingPlayer);
+  //   paper.addEventListener("click", () => {
+  //     setTimeout(resetJump, 200);
+  //   });
+  play();
+}
+game();
